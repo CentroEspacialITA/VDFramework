3428d6329ccd954861106b1c59a85f9cad242951
Matheus V. C. Cogo <mvccogo@gmail.com>
2022-12-14 16:10:43 -0300

    Transfer Dialog snippet

--- plugins/org.eclipse.cei.vdframework.core.handlers/src/org/eclipse/cei/vdframework/core/handlers/InitHandler.java
+++ plugins/org.eclipse.cei.vdframework.core.handlers/src/org/eclipse/cei/vdframework/core/handlers/InitHandler.java
@@ -2,6 +2,7 @@ package org.eclipse.cei.vdframework.core.handlers;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -18,6 +19,8 @@ import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionManager;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 import java.util.Collection;
+import org.polarsys.capella.core.sirius.analysis.actions.extensions.SelectElementsFromTransferWizard;
+import org.polarsys.capella.common.ui.toolkit.dialogs.TransferTreeListDialog;
 
 public class InitHandler extends AbstractHandler {
 
@@ -37,18 +40,38 @@ public class InitHandler extends AbstractHandler {
 		diag.setMessage("Please select the session (.aird file)");
 		diag.setTitle("[CEI-VD] Project Selection");
 		diag.setMultipleSelection(false);
-		Session result; 
+		Session result = null; 
 		if (diag.open() == Window.OK) {
 		   result =  (Session)diag.getFirstResult();
 		}
 		else {
 			return null;
 		}
-		
-		
-		
-		
-		
+		/*
+		Collection<EObject> current = (Collection) propertyContext.getCurrentValue(restraintProperty);
+        Collection<EObject> left = new HashSet<EObject>();
+        left.addAll((Collection) restraintProperty.getValue(propertyContext));
+        left.addAll((Collection) restraintProperty.getChoiceValues(propertyContext));
+        left.removeAll(current);
+        left.remove(null);
+        Collection<EObject> right = new HashSet<EObject>();
+        right.addAll(current);
+        right.remove(null);
+			*/
+        TransferTreeListDialog dialog = new TransferTreeListDialog(window.getShell(), "Selection wizard", "Select elements.");//$NON-NLS-2$
+        dialog.setLeftInput(new ArrayList<EObject>(), null);
+        dialog.setRightInput(new ArrayList<EObject>(), null);
+        if (dialog.open() == Window.OK) {
+          Object[] dialogResult = dialog.getResult();
+          ArrayList<Object> result2 = new ArrayList<Object>();
+          if (dialogResult != null) {
+            for (Object res : dialogResult) {
+              result2.add(res);
+            }
+          }
+          //changeValue(property, context, result);
+          //updatedValue(property, context, result);
+        }
 		
 		return null;
 	}
